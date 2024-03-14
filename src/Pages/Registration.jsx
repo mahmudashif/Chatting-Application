9e2f750e688cca84805be7593f88193aa521a32d
ashifmahmud <mdamahmud2000@gmail.com>
2024-03-15 00:52:34 +0600

    all setup done bro  and newly start registration part

--- src/Pages/Registration.jsx
+++ src/Pages/Registration.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Images from "../Images";
+import registrationImg from "../assets/registrationImage.png"
 
 const Registration = () => {
   return (
-    <div>Registration</div>
-  )
-}
+      <Grid container>
+        <Grid h1 xs={6}>
+          <h1 style={{background:"red"}}>xs=8</h1>
+        </Grid>
+        <Grid h1 xs={6}>
+            <Images src={registrationImg}/>
+        </Grid>
+      </Grid>
+  );
+};
 
-export default Registration
+export default Registration;
