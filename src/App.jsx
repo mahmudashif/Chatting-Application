25d79f957c839ad058e4c0194166918080366dc5
ashifmahmud <mdamahmud2000@gmail.com>
2024-03-14 23:46:01 +0600

    Router setup Done bro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-function App() {
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Registration from "./Pages/Registration";
+import Login from "./Pages/Login";
+import Home from "./Pages/Home";
 
-  return (
-    <>
-      <h1>Hello</h1>
-    </>
-  )
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Registration />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+      </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
