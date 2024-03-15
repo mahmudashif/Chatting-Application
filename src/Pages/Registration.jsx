7546982872547a7163f7feb83b3c9b09cd356a87
ashifmahmud <mdamahmud2000@gmail.com>
2024-03-15 22:50:01 +0600

    Email address part is complete and it's working

--- src/Pages/Registration.jsx
+++ src/Pages/Registration.jsx
@@ -3,29 +3,33 @@ import Grid from "@mui/material/Grid";
 import Images from "../Images";
 import registrationImg from "../assets/registrationImage.png";
 import TextField from "@mui/material/TextField";
-import Button from '@mui/material/Button';
-import Alert from '@mui/material/Alert';
+import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
 
 const Registration = () => {
-
-  let [registerData,setRegisterData] = useState({
+  let [registerData, setRegisterData] = useState({
     email: "",
     name: "",
-    password: ""
+    password: "",
   });
 
-  let handleChange = (e)=>{
-    setRegisterData({...registerData,[e.target.name]:e.target.value})
-    console.log(registerData);
-  }
+  let [registerError, setRegisterError] = useState({
+    email: "",
+    name: "",
+    password: "",
+  });
 
-  let handleSubmit = ()=>{
-    if (!setRegisterData.email) {
-      
+
+  let handleChange = (e) => {
+    setRegisterData({ ...registerData, [e.target.name]: e.target.value, });
+    setRegisterError({...registerError, email: ""})
+  };
+
+  let handleSubmit = () => {
+    if (!registerData.email) {
+      setRegisterError({ ...registerError, email: "Email Required" });
     }
-  }
-
-
+  };
 
   return (
     <Grid container>
@@ -34,44 +38,49 @@ const Registration = () => {
           Get started with easily register
         </h1>
         <div className="pt-[81px] pl-[193px]">
-        <TextField
-          name="email"
-          id="outlined-basic"
-          label="Email Address"
-          variant="outlined"
-          onChange={handleChange}
-        />
-        <Alert severity="error">Email Required.</Alert>
+          <TextField
+            name="email"
+            id="outlined-basic"
+            label="Email Address"
+            variant="outlined"
+            onChange={handleChange}
+            />
+            {registerError.email && 
+              <Alert severity="error">{registerError.email}</Alert>
+            }
         </div>
         <div className="pt-[33px] pl-[193px]">
-        <TextField
-          name="name"
-          required
-          id="outlined-required"
-          label="Full Name"
-          defaultValue=""
-          onChange={handleChange}
-        />
-          <Alert severity="error">Name Required.</Alert>
+          <TextField
+            name="name"
+            required
+            id="outlined-required"
+            label="Full Name"
+            defaultValue=""
+            onChange={handleChange}
+          />
+          <Alert severity="error">Name Required</Alert>
         </div>
         <div className="pt-[33px] pl-[193px]">
-        <TextField
-          name="password"
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          onChange={handleChange}
-        />
-        <Alert severity="error">Password Required.</Alert>
+          <TextField
+            name="password"
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            onChange={handleChange}
+          />
+          <Alert severity="error">Password Required.</Alert>
         </div>
         <div className="pt-[33px] pl-[193px]">
-        <Button onClick={handleSubmit} variant="contained" size="large">
-          Sign Up
-        </Button>
+          <Button onClick={handleSubmit} variant="contained" size="large">
+            Sign Up
+          </Button>
         </div>
         <div className="font-bold text-[13.34px] pt-8 pl-[193px]">
-            <p>Already  have an account ? <span className="text-[#EA6C00]">Sign In</span></p>
+          <p>
+            Already have an account ?{" "}
+            <span className="text-[#EA6C00]">Sign In</span>
+          </p>
         </div>
       </Grid>
       <Grid h1 xs={6}>
